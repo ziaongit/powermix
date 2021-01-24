92519811255740df6af6fcbd4963de957fff1118
Predicare\zia <zia.canic@predicare.se>
2021-01-24 21:46:07 +0100

    heart

--- src/components/SimilarSubscriptions.jsx
+++ src/components/SimilarSubscriptions.jsx
@@ -10,26 +10,34 @@ const Similar_Subscriptions = () => {
                         <img className="card-img-top" src="/img/team.png" alt="Team" />
                         <div className="card-body">
                             <div className="row mb-1">
-                            <div className="col-md-6 pro-name">Michael1234</div>
-                            <div className="col-md-6 address"><svg width="12" height="15" viewBox="0 0 12 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M11.3636 6.17401C11.3636 10.2043 6.18182 13.6589 6.18182 13.6589C6.18182 13.6589 1 10.2043 1 6.17401C1 4.7997 1.54594 3.48169 2.51772 2.50991C3.4895 1.53813 4.80751 0.992187 6.18182 0.992188C7.55612 0.992188 8.87414 1.53813 9.84592 2.50991C10.8177 3.48169 11.3636 4.7997 11.3636 6.17401Z" stroke="#8091A7" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round"/>
-                            <path d="M6.1804 7.90181C7.13434 7.90181 7.90767 7.12848 7.90767 6.17454C7.90767 5.22059 7.13434 4.44727 6.1804 4.44727C5.22645 4.44727 4.45312 5.22059 4.45312 6.17454C4.45312 7.12848 5.22645 7.90181 6.1804 7.90181Z" stroke="#8091A7" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round"/>
-                            </svg> United States</div>
+                                <div className="col-md-6 pro-name">Michael1234</div>
+                                <div className="col-md-6 address"><svg width="12" height="15" viewBox="0 0 12 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M11.3636 6.17401C11.3636 10.2043 6.18182 13.6589 6.18182 13.6589C6.18182 13.6589 1 10.2043 1 6.17401C1 4.7997 1.54594 3.48169 2.51772 2.50991C3.4895 1.53813 4.80751 0.992187 6.18182 0.992188C7.55612 0.992188 8.87414 1.53813 9.84592 2.50991C10.8177 3.48169 11.3636 4.7997 11.3636 6.17401Z" stroke="#8091A7" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
+                                    <path d="M6.1804 7.90181C7.13434 7.90181 7.90767 7.12848 7.90767 6.17454C7.90767 5.22059 7.13434 4.44727 6.1804 4.44727C5.22645 4.44727 4.45312 5.22059 4.45312 6.17454C4.45312 7.12848 5.22645 7.90181 6.1804 7.90181Z" stroke="#8091A7" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
+                                </svg> United States</div>
                             </div>
                             <div className="row mb-1">
-                            <div className="col-md-6 occupation">Web Developer</div>
-                            <div className="col-md-6 prise"><span className="usd">$233</span> /week</div>
+                                <div className="col-md-6 occupation">Web Developer</div>
+                                <div className="col-md-6 prise"><span className="usd">$233</span> /week</div>
                             </div>
                             <div className="row mb-1">
-                            <div className="col-md-12 skills">Laravel Nood.js PHP Python</div>
+                                <div className="col-md-12 skills">Laravel Nood.js PHP Python</div>
                             </div>
                             <div className="row mt-2">
-                            <div className="col-md-6 work"><img src="img/profile-work.png" alt="Profile Work"/> Works</div>
-                            <div className="col-md-6">
-                            <button className="subscribe">Subscribe</button>
-                            </div>
+                                <div className="col-md-6 work"><img src="img/profile-work.png" alt="Profile Work" /> Works</div>
+                                <div className="col-md-6">
+                                    <button className="subscribe">Subscribe</button>
+                                </div>
                             </div>
                         </div>
+                        <span className="like">
+                            <span className="heart">
+                                <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path fillRule="evenodd" clipRule="evenodd" d="M7.5 14.9922L1.09835 7.99219C-0.366116 6.39084 -0.366116 3.79454 1.09835 2.1932C2.56282 0.59185 4.93718 0.591849 6.40165 2.1932L7.5 3.39421L8.59835 2.1932C10.0628 0.59185 12.4372 0.591849 13.9016 2.1932C15.3661 3.79454 15.3661 6.39084 13.9017 7.99219L7.5 14.9922Z" fill="#FF484A" />
+                                </svg>
+                            </span>
+                            <span className="like-txt">314</span>
+                        </span>
                     </div>
                 </div>
                 <div className="col-md-3">
@@ -37,26 +45,34 @@ const Similar_Subscriptions = () => {
                         <img className="card-img-top" src="/img/team.png" alt="Team" />
                         <div className="card-body">
                             <div className="row mb-1">
-                            <div className="col-md-6 pro-name">Michael1234</div>
-                            <div className="col-md-6 address"><svg width="12" height="15" viewBox="0 0 12 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M11.3636 6.17401C11.3636 10.2043 6.18182 13.6589 6.18182 13.6589C6.18182 13.6589 1 10.2043 1 6.17401C1 4.7997 1.54594 3.48169 2.51772 2.50991C3.4895 1.53813 4.80751 0.992187 6.18182 0.992188C7.55612 0.992188 8.87414 1.53813 9.84592 2.50991C10.8177 3.48169 11.3636 4.7997 11.3636 6.17401Z" stroke="#8091A7" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round"/>
-                            <path d="M6.1804 7.90181C7.13434 7.90181 7.90767 7.12848 7.90767 6.17454C7.90767 5.22059 7.13434 4.44727 6.1804 4.44727C5.22645 4.44727 4.45312 5.22059 4.45312 6.17454C4.45312 7.12848 5.22645 7.90181 6.1804 7.90181Z" stroke="#8091A7" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round"/>
-                            </svg> United States</div>
+                                <div className="col-md-6 pro-name">Michael1234</div>
+                                <div className="col-md-6 address"><svg width="12" height="15" viewBox="0 0 12 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M11.3636 6.17401C11.3636 10.2043 6.18182 13.6589 6.18182 13.6589C6.18182 13.6589 1 10.2043 1 6.17401C1 4.7997 1.54594 3.48169 2.51772 2.50991C3.4895 1.53813 4.80751 0.992187 6.18182 0.992188C7.55612 0.992188 8.87414 1.53813 9.84592 2.50991C10.8177 3.48169 11.3636 4.7997 11.3636 6.17401Z" stroke="#8091A7" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
+                                    <path d="M6.1804 7.90181C7.13434 7.90181 7.90767 7.12848 7.90767 6.17454C7.90767 5.22059 7.13434 4.44727 6.1804 4.44727C5.22645 4.44727 4.45312 5.22059 4.45312 6.17454C4.45312 7.12848 5.22645 7.90181 6.1804 7.90181Z" stroke="#8091A7" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
+                                </svg> United States</div>
                             </div>
                             <div className="row mb-1">
-                            <div className="col-md-6 occupation">Web Developer</div>
-                            <div className="col-md-6 prise"><span className="usd">$233</span> /week</div>
+                                <div className="col-md-6 occupation">Web Developer</div>
+                                <div className="col-md-6 prise"><span className="usd">$233</span> /week</div>
                             </div>
                             <div className="row mb-1">
-                            <div className="col-md-12 skills">Laravel Nood.js PHP Python</div>
+                                <div className="col-md-12 skills">Laravel Nood.js PHP Python</div>
                             </div>
                             <div className="row mt-2">
-                            <div className="col-md-6 work"><img src="img/profile-work.png" alt="Profile Work"/> Works</div>
-                            <div className="col-md-6">
-                            <button className="subscribe">Subscribe</button>
-                            </div>
+                                <div className="col-md-6 work"><img src="img/profile-work.png" alt="Profile Work" /> Works</div>
+                                <div className="col-md-6">
+                                    <button className="subscribe">Subscribe</button>
+                                </div>
                             </div>
                         </div>
+                        <span className="like">
+                            <span className="heart">
+                                <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path fillRule="evenodd" clipRule="evenodd" d="M7.5 14.9922L1.09835 7.99219C-0.366116 6.39084 -0.366116 3.79454 1.09835 2.1932C2.56282 0.59185 4.93718 0.591849 6.40165 2.1932L7.5 3.39421L8.59835 2.1932C10.0628 0.59185 12.4372 0.591849 13.9016 2.1932C15.3661 3.79454 15.3661 6.39084 13.9017 7.99219L7.5 14.9922Z" fill="#FF484A" />
+                                </svg>
+                            </span>
+                            <span className="like-txt">314</span>
+                        </span>
                     </div>
                 </div>
                 <div className="col-md-3">
@@ -64,26 +80,34 @@ const Similar_Subscriptions = () => {
                         <img className="card-img-top" src="/img/team.png" alt="Team" />
                         <div className="card-body">
                             <div className="row mb-1">
-                            <div className="col-md-6 pro-name">Michael1234</div>
-                            <div className="col-md-6 address"><svg width="12" height="15" viewBox="0 0 12 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M11.3636 6.17401C11.3636 10.2043 6.18182 13.6589 6.18182 13.6589C6.18182 13.6589 1 10.2043 1 6.17401C1 4.7997 1.54594 3.48169 2.51772 2.50991C3.4895 1.53813 4.80751 0.992187 6.18182 0.992188C7.55612 0.992188 8.87414 1.53813 9.84592 2.50991C10.8177 3.48169 11.3636 4.7997 11.3636 6.17401Z" stroke="#8091A7" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round"/>
-                            <path d="M6.1804 7.90181C7.13434 7.90181 7.90767 7.12848 7.90767 6.17454C7.90767 5.22059 7.13434 4.44727 6.1804 4.44727C5.22645 4.44727 4.45312 5.22059 4.45312 6.17454C4.45312 7.12848 5.22645 7.90181 6.1804 7.90181Z" stroke="#8091A7" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round"/>
-                            </svg> United States</div>
+                                <div className="col-md-6 pro-name">Michael1234</div>
+                                <div className="col-md-6 address"><svg width="12" height="15" viewBox="0 0 12 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M11.3636 6.17401C11.3636 10.2043 6.18182 13.6589 6.18182 13.6589C6.18182 13.6589 1 10.2043 1 6.17401C1 4.7997 1.54594 3.48169 2.51772 2.50991C3.4895 1.53813 4.80751 0.992187 6.18182 0.992188C7.55612 0.992188 8.87414 1.53813 9.84592 2.50991C10.8177 3.48169 11.3636 4.7997 11.3636 6.17401Z" stroke="#8091A7" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
+                                    <path d="M6.1804 7.90181C7.13434 7.90181 7.90767 7.12848 7.90767 6.17454C7.90767 5.22059 7.13434 4.44727 6.1804 4.44727C5.22645 4.44727 4.45312 5.22059 4.45312 6.17454C4.45312 7.12848 5.22645 7.90181 6.1804 7.90181Z" stroke="#8091A7" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
+                                </svg> United States</div>
                             </div>
                             <div className="row mb-1">
-                            <div className="col-md-6 occupation">Web Developer</div>
-                            <div className="col-md-6 prise"><span className="usd">$233</span> /week</div>
+                                <div className="col-md-6 occupation">Web Developer</div>
+                                <div className="col-md-6 prise"><span className="usd">$233</span> /week</div>
                             </div>
                             <div className="row mb-1">
-                            <div className="col-md-12 skills">Laravel Nood.js PHP Python</div>
+                                <div className="col-md-12 skills">Laravel Nood.js PHP Python</div>
                             </div>
                             <div className="row mt-2">
-                            <div className="col-md-6 work"><img src="img/profile-work.png" alt="Profile Work"/> Works</div>
-                            <div className="col-md-6">
-                            <button className="subscribe">Subscribe</button>
-                            </div>
+                                <div className="col-md-6 work"><img src="img/profile-work.png" alt="Profile Work" /> Works</div>
+                                <div className="col-md-6">
+                                    <button className="subscribe">Subscribe</button>
+                                </div>
                             </div>
                         </div>
+                        <span className="like">
+                            <span className="heart">
+                                <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path fillRule="evenodd" clipRule="evenodd" d="M7.5 14.9922L1.09835 7.99219C-0.366116 6.39084 -0.366116 3.79454 1.09835 2.1932C2.56282 0.59185 4.93718 0.591849 6.40165 2.1932L7.5 3.39421L8.59835 2.1932C10.0628 0.59185 12.4372 0.591849 13.9016 2.1932C15.3661 3.79454 15.3661 6.39084 13.9017 7.99219L7.5 14.9922Z" fill="#FF484A" />
+                                </svg>
+                            </span>
+                            <span className="like-txt">314</span>
+                        </span>
                     </div>
                 </div>
                 <div className="col-md-3">
@@ -91,26 +115,34 @@ const Similar_Subscriptions = () => {
                         <img className="card-img-top" src="/img/team.png" alt="Team" />
                         <div className="card-body">
                             <div className="row mb-1">
-                            <div className="col-md-6 pro-name">Michael1234</div>
-                            <div className="col-md-6 address"><svg width="12" height="15" viewBox="0 0 12 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M11.3636 6.17401C11.3636 10.2043 6.18182 13.6589 6.18182 13.6589C6.18182 13.6589 1 10.2043 1 6.17401C1 4.7997 1.54594 3.48169 2.51772 2.50991C3.4895 1.53813 4.80751 0.992187 6.18182 0.992188C7.55612 0.992188 8.87414 1.53813 9.84592 2.50991C10.8177 3.48169 11.3636 4.7997 11.3636 6.17401Z" stroke="#8091A7" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round"/>
-                            <path d="M6.1804 7.90181C7.13434 7.90181 7.90767 7.12848 7.90767 6.17454C7.90767 5.22059 7.13434 4.44727 6.1804 4.44727C5.22645 4.44727 4.45312 5.22059 4.45312 6.17454C4.45312 7.12848 5.22645 7.90181 6.1804 7.90181Z" stroke="#8091A7" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round"/>
-                            </svg> United States</div>
+                                <div className="col-md-6 pro-name">Michael1234</div>
+                                <div className="col-md-6 address"><svg width="12" height="15" viewBox="0 0 12 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M11.3636 6.17401C11.3636 10.2043 6.18182 13.6589 6.18182 13.6589C6.18182 13.6589 1 10.2043 1 6.17401C1 4.7997 1.54594 3.48169 2.51772 2.50991C3.4895 1.53813 4.80751 0.992187 6.18182 0.992188C7.55612 0.992188 8.87414 1.53813 9.84592 2.50991C10.8177 3.48169 11.3636 4.7997 11.3636 6.17401Z" stroke="#8091A7" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
+                                    <path d="M6.1804 7.90181C7.13434 7.90181 7.90767 7.12848 7.90767 6.17454C7.90767 5.22059 7.13434 4.44727 6.1804 4.44727C5.22645 4.44727 4.45312 5.22059 4.45312 6.17454C4.45312 7.12848 5.22645 7.90181 6.1804 7.90181Z" stroke="#8091A7" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
+                                </svg> United States</div>
                             </div>
                             <div className="row mb-1">
-                            <div className="col-md-6 occupation">Web Developer</div>
-                            <div className="col-md-6 prise"><span className="usd">$233</span> /week</div>
+                                <div className="col-md-6 occupation">Web Developer</div>
+                                <div className="col-md-6 prise"><span className="usd">$233</span> /week</div>
                             </div>
                             <div className="row mb-1">
-                            <div className="col-md-12 skills">Laravel Nood.js PHP Python</div>
+                                <div className="col-md-12 skills">Laravel Nood.js PHP Python</div>
                             </div>
                             <div className="row mt-2">
-                            <div className="col-md-6 work"><img src="img/profile-work.png" alt="Profile Work"/> Works</div>
-                            <div className="col-md-6">
-                            <button className="subscribe">Subscribe</button>
-                            </div>
+                                <div className="col-md-6 work"><img src="img/profile-work.png" alt="Profile Work" /> Works</div>
+                                <div className="col-md-6">
+                                    <button className="subscribe">Subscribe</button>
+                                </div>
                             </div>
                         </div>
+                        <span className="like">
+                            <span className="heart">
+                                <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path fillRule="evenodd" clipRule="evenodd" d="M7.5 14.9922L1.09835 7.99219C-0.366116 6.39084 -0.366116 3.79454 1.09835 2.1932C2.56282 0.59185 4.93718 0.591849 6.40165 2.1932L7.5 3.39421L8.59835 2.1932C10.0628 0.59185 12.4372 0.591849 13.9016 2.1932C15.3661 3.79454 15.3661 6.39084 13.9017 7.99219L7.5 14.9922Z" fill="#FF484A" />
+                                </svg>
+                            </span>
+                            <span className="like-txt">314</span>
+                        </span>
                     </div>
                 </div>
             </div>
